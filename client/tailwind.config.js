01799fa3da42f94a9d9eb5f4805c440cd020d6ca
ayushgitt <ayush795592@gmail.com>
2024-11-30 15:16:22 +0530

    modified:   client/src/components/Footer.jsx 	modified:   client/tailwind.config.js 	deleted:    server/env

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import("tailwindcss").Config} */
-
 module.exports = {
     content: [
-        './src/**/*.{html,js,jsx,ts,tsx}'
+      './src/**/*.{html,js,jsx,ts,tsx}'
     ],
     theme: {
-        extend: {},
+      extend: {
+        screens: {
+          'custom-sm': '400px',
+        },
+      },
     },
     plugins: [],
-};
+  };
